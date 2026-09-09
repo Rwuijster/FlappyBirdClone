a9ccc865b0e74b2186ed497e9b72ed838d5b9b29
Rwuijster Dev <wuijster.rick@gmail.com>
2026-09-09 10:25:16 +0200

    added flappy

--- flappybird.js
+++ flappybird.js
@@ -8,11 +8,15 @@ let context;
 //variables for the bird
 let birdWidth = 34; // 34/24 is the ratio
 let birdHeight = 24;
-let birdXPosition: boardWidth / 8;
-let birdYPosition: boardHeight / 2;
+let birdXPosition = boardWidth / 8;
+let birdYPosition = boardHeight / 2;
+let birdImg;
 
 let Bird = {
-	
+	x : birdXPosition,
+	y : birdYPosition,
+	width: birdWidth,
+	height: birdHeight,	
 }
 
 //Sets the board dimensions to the one stated in the top
@@ -21,4 +25,14 @@ window.onload = function() {
 	board.height = boardHeight;
 	board.width = boardWidth;
 	context = board.getContext("2d"); //used to draw on the board
+
+	//draw flappy bird
+	context.fillStyle = "green";
+	context.fillRect(birdXPosition, birdYPosition, birdWidth, birdHeight);
+
+	birdImg = new Image();
+	birdImg.src = "./flappybird.png";
+	birdImg.onload = function() {
+		context.drawImage(birdImg, Bird.x, Bird.y, Bird.width, Bird.height);
+	}
 }
