dd1938f18e80f3612d2ab334096a1389ac7cdbb9
Rwuijster Dev <wuijster.rick@gmail.com>
2026-09-09 12:11:34 +0200

    push the pipes

--- flappybird.js
+++ flappybird.js
@@ -19,6 +19,17 @@ let Bird = {
 	height: birdHeight,	
 }
 
+//variables for pipes
+let pipeArray = [];
+let pipeWidth = 64; // keep this ratio
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+
+let topPipeImg;
+let bottomPipeImg;
+
 //Sets the board dimensions to the one stated in the top
 window.onload = function() {
 	board = document.getElementById("board");
@@ -26,13 +37,51 @@ window.onload = function() {
 	board.width = boardWidth;
 	context = board.getContext("2d"); //used to draw on the board
 
-	//draw flappy bird/
-	context.fillStyle = "green";
-	context.fillRect(birdXPosition, birdYPosition, birdWidth, birdHeight);
+	//draw flappy bird
+	//context.fillStyle = "green";
+	//context.fillRect(birdXPosition, birdYPosition, birdWidth, birdHeight);
 
+	//load images
 	birdImg = new Image();
 	birdImg.src = "./flappybird.png";
 	birdImg.onload = function() {
 		context.drawImage(birdImg, Bird.x, Bird.y, Bird.width, Bird.height);
 	}
+
+	topPipeImg = new Image();
+	topPipeImg.src = "./toppipe.png";
+
+	bottomPipeImg = new Image();
+	bottomPipeImg.src = "./bottompipe.png";
+
+	requestAnimationFrame(update);
+	setInterval(placePipes, 1500);
+}
+
+function update() {
+	requestAnimationFrame(update);
+	context.clearRect(0,0, board.width, board.height);
+
+	//bird
+	context.drawImage(birdImg, Bird.x, Bird.y, Bird.width, Bird.height);
+
+	//pipes
+	for (let i = 0; i < pipeArray.length; i++) {
+		let pipe = pipeArray[i];
+		context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+	}
+}
+
+function placePipes() {
+
+	let topPipe = {
+		img : topPipeImg,
+		x : pipeX,
+		y : pipeY,
+		width : pipeWidth,
+		height : pipeHeight,
+		passed : false
+	}
+
+	pipeArray.push(topPipe);
 }
